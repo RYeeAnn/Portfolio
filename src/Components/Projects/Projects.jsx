631d9774d7fad94c7349c70fdc137e4d7623243d
Ryan <ryeean16@gmail.com>
2023-10-12 17:06:12 -0700

    removed link on projects when clicking on image and added linked below

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -5,7 +5,8 @@ import cruisin from '../../assets/cruisin.png';
 import cruisinVideo from '../../assets/cruisinVideo.mp4'
 import brainflix from '../../assets/brainflix.png';
 import bandsite from '../../assets/bandsite.png';
-import instock from '../../assets/instock.png'
+import instock from '../../assets/instock.png';
+import shnakeGif from '../../assets/shnake-gif.gif';
 
 function Projects() {
   return (
@@ -24,24 +25,38 @@ function Projects() {
               <a href="https://cruisin.netlify.app/"><h3>Cruisin'</h3></a>
               <p>My first full-stack project I created with the purpose of helping driver's with their vehicle concerns.</p>
               <p>Deployed front-end using Netlify and back-end using Heroku.</p>
-              <p><b>React, Express, MySQL</b></p>
+              <p><b>React, Express, Sass, MySQL</b></p>
+              <p><a href="https://github.com/RYeeAnn/brainstation-capstone">Click here for front-end repo</a></p>
+              <p><a href="https://github.com/RYeeAnn/brainstation-capstone-server">Click here for back-end repo</a></p>
             </div>
           </div>
 
           {/* Card 2 */}
           <div className="projects__card">
-            <a href="https://github.com/RYeeAnn/brainstation-BrainFlix"><img src={brainflix} alt="Project 2" className="projects__image" /></a>
+            <img src={shnakeGif} alt="Project 2" className="projects__image" />
             <div className="projects__info">
-              <h3>BrainFlix</h3>
-              <p>BrainFlix is a full-stack, fully responsive project built to mimic youtube during my enrollment in BrainStation's intensive web developement bootcamp.</p>
-              <p><b>React, Express, JSON</b></p>
-              <p><a href="https://github.com/RYeeAnn/brainstation-BrainFlix-api">Click here for the back-end repo</a></p>
+              <h3>Shnake</h3>
+              <p>A simple snake game inspired by classic games that was played as a kid. Created and coded using Javascript and React.js.</p>
+              <p><b>JavaScript, React, Sass</b></p>
+              <p><a href="https://github.com/RYeeAnn/Shnake">Click here for github repo</a></p>
             </div>
           </div>
 
           {/* Card 3 */}
           <div className="projects__card">
-            <a href="https://github.com/RYeeAnn/brainstation-bandsite"><img src={bandsite} alt="Project 3" className="projects__image" /></a>
+            <img src={brainflix} alt="Project 3" className="projects__image" />
+            <div className="projects__info">
+              <h3>BrainFlix</h3>
+              <p>BrainFlix is a full-stack, fully responsive project built to mimic youtube during my enrollment in BrainStation's intensive web developement bootcamp.</p>
+              <p><b>React, Express, Sass, JSON</b></p>
+              <p><a href="https://github.com/RYeeAnn/brainstation-BrainFlix">Click here for the front-end repo</a></p>
+              <p><a href="https://github.com/RYeeAnn/brainstation-BrainFlix-api">Click here for the back-end repo</a></p>
+            </div>
+          </div>
+
+          {/* Card 4 */}
+          <div className="projects__card">
+            <img src={bandsite} alt="Project 4" className="projects__image" />
             <div className="projects__info">
               <h3>BandSite</h3>
               <p>BandSite is a fully responsive project built to mimic a fanpage for a mock band, built during my enrollment in BrainStation's intensive web development bootcamp.</p>
@@ -50,13 +65,14 @@ function Projects() {
             </div>
           </div>
 
-          {/* Card 4 */}
+          {/* Card 5 */}
           <div className="projects__card">
-            <a href="https://github.com/RYeeAnn/brainstation-instock"><img src={instock} alt="Project 4" className="projects__image" /></a>
+            <img src={instock} alt="Project 5" className="projects__image" />
             <div className="projects__info">
               <h3>Instock</h3>
               <p>Instock is a full-stack, fully responsive collaborative project with a team of 3 developers built to mimic a warehouse and inventory tracker.</p>
               <p><b>React, Express, MySQL</b></p>
+              <p><a href="https://github.com/RYeeAnn/brainstation-instock">Click here for the front-end repo</a></p>
               <p><a href="https://github.com/RYeeAnn/brainstation-instock-server">Click here for the back-end repo</a></p>
             </div>
           </div>
