29e9544399a076961c4349b5facc3f70635b493f
Ryan <ryeean16@gmail.com>
2023-11-09 02:28:23 -0800

    added text still in progress for cafe project

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -38,6 +38,7 @@ function Projects() {
             <img src={theCozyCorner} alt="Project 2" className="projects__image" />
             <div className="projects__info">
               <a href="https://thecozycorner-0537f656a932.herokuapp.com/"><h3>The Cozy Corner</h3></a>
+              <p>Still in progress . . .</p>
               <p>The Cozy Corner is a website designed to feel just like your favorite neighborhood coffee shop. It's a welcoming online spot where customers can check out a handpicked selection of delicious coffees, homemade pastries, and tasty snacks.</p>
               <p><b>HTML, CSS, Python, Flask, MongolDB, Javascript</b></p>
               <p><a href="https://github.com/RYeeAnn/Ryan-Cafe">Click here for github repo</a></p>
