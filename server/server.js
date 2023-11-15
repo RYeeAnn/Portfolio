77f7916afc545bf56d5547578090068c5e363fcf
Ryan <ryeean16@gmail.com>
2023-11-14 17:58:15 -0800

    installed axios

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mongoose = require('mongoose'); // Corrected this line
+const mongoose = require('mongoose');
 const app = express();
 const cors = require('cors');
 const axios = require('axios');
