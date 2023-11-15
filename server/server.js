53b4c079475d251360ca264b60c958594410a14a
Ryan <ryeean16@gmail.com>
2023-11-14 17:50:54 -0800

    api key update

--- server/server.js
+++ server/server.js
@@ -9,11 +9,15 @@ if (process.env.NODE_ENV !== 'production') {
 
 // Middleware to parse JSON bodies
 app.use(cors());
-
 app.use(express.json());
 
 const mongoURI = process.env.MONGO_URI;
 const port = process.env.PORT || 5001;
+const contactForm = process.env.DATA_API_KEY;
+const contactFormEndPoint = process.env.DATA_API_ENDPOINT;
+const contactSource = process.env.DATA_SOURCE;
+const contactDatabase = process.env.DATABASE_NAME;
+const contactCollection = process.env.COLLECTION_NAME;
 
 // Connect to MongoDB
 mongoose.connect(mongoURI)
@@ -32,14 +36,29 @@ const Contact = mongoose.model('Contact', contactSchema);
 app.post('/submit_contact', (req, res) => {
     const { name, email, message } = req.body;
 
-    const newContact = new Contact({ name, email, message });
-    
-    newContact.save()
-        .then(() => res.send('Form data received and saved.'))
-        .catch(err => {
-            console.error("Error saving data:", err);
+
+    axios.post(`${contactFormEndPoint}/action/insertOne`, {
+        dataSource: contactSource,
+        database: contactDatabase,
+        collection: contactCollection,
+        document: { name, email, message }
+    }, {
+        headers: {
+            "Content-Type": "application/json",
+            "api-key": contactForm
+        }
+    })
+    .then(response => {
+        if (response.status === 200) {
+            res.send('Form data received and saved.');
+        } else {
             res.status(500).send('Error saving data.');
-        });
+        }
+    })
+    .catch(err => {
+        console.error("Error saving data:", err);
+        res.status(500).send('Error saving data.');
+    });
 });
 
 app.listen(port, () => {
