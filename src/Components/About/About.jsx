17d677f9590b2129f59295ca7fae28d5dcdf7307
Ryan <ryeean16@gmail.com>
2024-03-11 02:48:56 -0700

    updated resume with internship

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -10,7 +10,7 @@ import github from '../../assets/github.png';
 
 function About() {
 
-    const resumeLink = "https://drive.google.com/file/d/1tGRmo3OAcDLcZoqsdkAjz_rAeDGS3ez2/view?usp=sharing";
+    const resumeLink = "https://drive.google.com/file/d/1uGkvnYytZP8uKQiuWh0DFZfnNXcfhnva/view?usp=sharing";
     const linkedinLink = "https://www.linkedin.com/in/ryeean/";
     const githubLink = "https://github.com/RYeeAnn";
 
